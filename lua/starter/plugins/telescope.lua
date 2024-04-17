7d08c7c45336ce544c6e53fc07b5b024d3883e96
Frédéric Ruaudel <github@fredericruaudel.fr>
2024-04-17 15:12:37 +0200

    Improve nvim config following ThePrimeagen 0 to LSP video

--- lua/starter/plugins/telescope.lua
+++ lua/starter/plugins/telescope.lua
@@ -8,18 +8,23 @@ return {
     local telescope = require("telescope")
 
     telescope.setup({
-        ensure_installed = { "swift" }
+      ensure_installed = { "swift" },
     })
 
     -- set keymaps
     local keymap = vim.keymap -- for conciseness
+    local builtin = require("telescope.builtin")
 
     keymap.set(
       "n",
       "<leader>ff",
-      "<cmd>Telescope find_files theme=dropdown previewer=false<cr>",
-      { desc = "Fuzzy find files in cwd" }
+      builtin.find_files,
+      { desc = "Fuzzy find all files in cwd" }
     )
-    keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Live grep in cwd" })
+    -- keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep in cwd" })
+    keymap.set("n", "<leader>fg", function()
+      builtin.grep_string({ search = vim.fn.input("Grep > ") })
+    end, { desc = "Live grep in cwd" })
+    keymap.set("n", "<C-p>", builtin.git_files, { desc = "Fuzzy find git file search" })
   end,
 }
