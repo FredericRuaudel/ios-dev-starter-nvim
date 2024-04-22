09f4913a6b438b2d331f6c018d4c9ddf6001194c
Frédéric Ruaudel <github@fredericruaudel.fr>
2024-04-22 18:16:18 +0200

    add autopairs and refactor telescope config

--- lua/starter/plugins/telescope.lua
+++ lua/starter/plugins/telescope.lua
@@ -15,13 +15,7 @@ return {
     local keymap = vim.keymap -- for conciseness
     local builtin = require("telescope.builtin")
 
-    keymap.set(
-      "n",
-      "<leader>ff",
-      builtin.find_files,
-      { desc = "Fuzzy find all files in cwd" }
-    )
-    -- keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep in cwd" })
+    keymap.set( "n", "<leader>ff", builtin.find_files, { desc = "Fuzzy find all files in cwd" })
     keymap.set("n", "<leader>fg", function()
       builtin.grep_string({ search = vim.fn.input("Grep > ") })
     end, { desc = "Live grep in cwd" })
